1aaa4916ec3017cb4dea4ac71cf7c4147bb6ab8f
João Pedro Gomes de Souza <pedrodaserenidade@gmail.com>
2019-06-04 15:18:55 -0300

    Mudanças na Classe Catalogo.java
    novos métodos implementados e mudança no construtor.

--- Loja Virtual/src/loja/virtual/Catalogo.java
+++ Loja Virtual/src/loja/virtual/Catalogo.java
@@ -3,30 +3,71 @@ import java.util.ArrayList;
 import java.util.ArrayList;
 
 public class Catalogo {
-    ArrayList<Produtos> stock = new ArrayList();
+    ArrayList <Produto>estoque = new ArrayList<>();
     
-    Produtos Camisa;
-    Produtos Notebook;
-    Produtos Geladeira;
-
-    public Catalogo() {
-        this.Camisa = new Produtos(1, "Camisa", 50.00, 20);
-        this.Notebook = new Produtos(2, "Notebook", 3000.0, 10);
-        this.Geladeira = new Produtos(3, "Geladeira", 2000.0, 8);
-        
-        stock.add(Camisa);
-        stock.add(Notebook);
-        stock.add(Geladeira);
+    public Catalogo(ArrayList <Produto>estoque){
+        this.estoque = estoque;
     }
-
     
     public void Listar(){
-        for(int i = 0; i < stock.size(); i++){
-              System.out.println("ID: " + stock.get(i).ID);
-              System.out.println("Descrição: " + stock.get(i).Descricao);
-              System.out.println("Valor: " + stock.get(i).Valor);
-              System.out.println("Quantidade no estoque: " + stock.get(i).QntEstoque);
-              System.out.println();
-        }
+            for (Produto obj : estoque) {
+                System.out.println("NOME: " + obj.getName());
+                System.out.println("CATEGORIA: " + obj.getCategory());
+                System.out.println("DESCRIÇÃO: " + obj.getDescription());
+                String resultado = String.format("%.2f", obj.getPrice()); // formatação do preço
+                System.out.println("PRECÇO: R$" + resultado);
+                System.out.println("QUANTIDADE EM ESTOQUE: " + obj.getAmount());
+                System.out.println("===============================================");
+            }
+    }
+    
+    public ArrayList <Produto> filtrarCategoria(ArrayList <Produto>estoque){
+            ArrayList <Produto>filtro = new ArrayList<>();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Selecione a categoria: "); // menuzinho
+            System.out.println("1 - Vestuário");
+            System.out.println("2 - Jogos");
+            System.out.println("3 - Colecionáveis");
+            System.out.println("4 - Decoração");
+            System.out.println("5 - Leitura");
+            int choice = input.nextInt();
+            switch(choice){
+                case 1:
+                    for(Produto obj: estoque){
+                        if(obj.getCategory().equals("Vestuário")){
+                            filtro.add(obj);
+                        }
+                    }
+                    break;
+                case 2:
+                    for(Produto obj: estoque){
+                        if(obj.getCategory().equals("Jogos")){
+                            filtro.add(obj);
+                        }
+                    }
+                    break;
+                case 3:
+                    for(Produto obj: estoque){
+                        if(obj.getCategory().equals("Colecionáveis")){
+                            filtro.add(obj);
+                        }
+                    }
+                    break;
+                case 4:
+                    for(Produto obj: estoque){
+                        if(obj.getCategory().equals("Decoração")){
+                            filtro.add(obj);
+                        }
+                    }
+                    break;
+                case 5:
+                    for(Produto obj: estoque){
+                        if(obj.getCategory().equals("Leitura")){
+                            filtro.add(obj);
+                        }
+                    }
+                    break;
+            }
+            return filtro;
     }
 }
