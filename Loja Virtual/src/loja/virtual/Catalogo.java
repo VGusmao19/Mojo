d729660c7b5370449a2575189b837c6eb547f276
João Pedro Gomes de Souza <pedrodaserenidade@gmail.com>
2019-06-05 15:19:07 -0300

    Update Catalogo.java

--- Loja Virtual/src/loja/virtual/Catalogo.java
+++ Loja Virtual/src/loja/virtual/Catalogo.java
@@ -1,17 +1,17 @@
-package loja.virtual;
 import java.util.ArrayList;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Catalogo {
-    ArrayList <Produto>estoque = new ArrayList<>();
+    ArrayList <Produtos>estoque = new ArrayList<>();
     
-    public Catalogo(ArrayList <Produto>estoque){
+    public Catalogo(ArrayList <Produtos>estoque){
         this.estoque = estoque;
     }
     
     public void Listar(){
-            for (Produto obj : estoque) {
+            for (Produtos obj : estoque) {
                 System.out.println("NOME: " + obj.getName());
+                System.out.println("ID: " + obj.getID());
                 System.out.println("CATEGORIA: " + obj.getCategory());
                 System.out.println("DESCRIÇÃO: " + obj.getDescription());
                 String resultado = String.format("%.2f", obj.getPrice()); // formatação do preço
@@ -21,8 +21,8 @@ public class Catalogo {
             }
     }
     
-    public ArrayList <Produto> filtrarCategoria(ArrayList <Produto>estoque){
-            ArrayList <Produto>filtro = new ArrayList<>();
+    public ArrayList <Produtos> filtrarCategoria(ArrayList <Produtos>estoque){
+            ArrayList <Produtos>filtro = new ArrayList<>();
             Scanner input = new Scanner(System.in);
             System.out.println("Selecione a categoria: "); // menuzinho
             System.out.println("1 - Vestuário");
@@ -33,35 +33,35 @@ public class Catalogo {
             int choice = input.nextInt();
             switch(choice){
                 case 1:
-                    for(Produto obj: estoque){
+                    for(Produtos obj: estoque){
                         if(obj.getCategory().equals("Vestuário")){
                             filtro.add(obj);
                         }
                     }
                     break;
                 case 2:
-                    for(Produto obj: estoque){
+                    for(Produtos obj: estoque){
                         if(obj.getCategory().equals("Jogos")){
                             filtro.add(obj);
                         }
                     }
                     break;
                 case 3:
-                    for(Produto obj: estoque){
+                    for(Produtos obj: estoque){
                         if(obj.getCategory().equals("Colecionáveis")){
                             filtro.add(obj);
                         }
                     }
                     break;
                 case 4:
-                    for(Produto obj: estoque){
+                    for(Produtos obj: estoque){
                         if(obj.getCategory().equals("Decoração")){
                             filtro.add(obj);
                         }
                     }
                     break;
                 case 5:
-                    for(Produto obj: estoque){
+                    for(Produtos obj: estoque){
                         if(obj.getCategory().equals("Leitura")){
                             filtro.add(obj);
                         }
@@ -69,5 +69,21 @@ public class Catalogo {
                     break;
             }
             return filtro;
+    }
+    
+    public void Pesquisar(String id){
+        for(Produtos obj : estoque){
+            if(obj.getID().equals(id)){
+                System.out.println("ID: " + obj.getID());
+                System.out.println("NOME: " + obj.getName());
+                System.out.println("CATEGORIA: " + obj.getCategory());
+                System.out.println("DESCRIÇÃO: " + obj.getDescription());
+                String resultado = String.format("%.2f", obj.getPrice()); // formatação do preço
+                System.out.println("PRECÇO: R$" + resultado);
+                System.out.println("QUANTIDADE EM ESTOQUE: " + obj.getAmount());
+                System.out.println("===============================================");
+            }
+            
+        }
     }
 }
